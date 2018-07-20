50d7f09a157f70ee09d82d305816740ecb1a5cec
Angela Andrews <aandrews17@yahoo.com>
2018-07-20 10:03:57 -0400

    OMDB search working. Default movie works

--- liri.js
+++ liri.js
@@ -17,19 +17,26 @@ var separator = '---------------------------------------------';
 
 
 ////////////////////// functions //////////////////////
-///same old error call back
-// function gotData(err, data,response){
-//     console.log(data)
-// };
+//write to file function
 
-//Show last 20 tweets and when they were created
+var appendToFile = function(data){
+
+    fs.appendFile('log.txt', data+'\n', function(err){
+        if (err) {
+            console.log(err);
+        } 
+    });
+};
+
+/////////////////////////////// Tweets Function ///////////////////////
+
 function showTweets(){
     
 var client = new twitter(keys.twitter);
     console.log(`tweet function.`)
     var params = {
         screen_name: 'scoutypouty1',
-        count: 2
+        count: 5
     }
 
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -46,26 +53,30 @@ var client = new twitter(keys.twitter);
 
             console.log(logText);
            
-            fs.appendFile('log.txt', logText+'\n', function(err){
-                if (err) {
-                    console.log(err);
-                } 
-    
-            });
+          appendToFile(logText);
         }
          
         }
       });
 };
 
+/////////////////////End tweet Function ///////////////////////
 
-//Show information about a song  Pass on in
+////////////////////////  spotify Function ////////////////////////////
 function spotifySong(){
-    //console.log("Song Info here");
-    var spotify = new Spotify(keys.spotify);
-       
+    console.log("Song Info here");
     var myQuery = process.argv[3];
-
+    // console.log(myQuery===undefined);
+    // return;
+    if (myQuery === undefined ) {
+        myQuery = 'apeshit';
+        // console.log(myQuery);
+        // return;
+        }
+        
+    var spotify = new Spotify(keys.spotify);  
+   // var myQuery = process.argv[3];
+   
     spotify.search({ type: 'track', query: myQuery, limit: 1 }, function(err, data) {
       if (err) {
         return console.log('Error occurred: ' + err);
@@ -77,15 +88,57 @@ function spotifySong(){
     var songName = JSON.stringify(info.name);
     var albumName  = JSON.stringify(info.album.name);
     var trackUrl = JSON.stringify(info.external_urls.spotify);
+    var logTextSpotify = `Artist(s) Name: ${artistName}\nSong Title: ${songName}\nListen Here: ${trackUrl}\nAlbum Name: ${albumName}\n${separator}`;
+
+    console.log(logTextSpotify);
     
-    console.log(`Artist(s) Name: ${artistName}\nSong Title: ${songName}\nListen Here: ${trackUrl}\nAlbum Name: ${albumName}`)
-    
+    appendToFile(logTextSpotify);
     
     });
+ 
 };
+///////////////////// spotify  Function ends///////////////////////
 
+/////////////////////////////// Movie Function ///////////////////////
 function movieInfo(){
-    console.log("Movie info here");
+    //console.log("Movie info here");
+    var title = process.argv[3];
+if(title ===undefined){
+    title="Malcolm X";
+     console.log(`Default Title Query:`);
+     console.log(`***********************************************`);
+};
+var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+
+
+request(queryURL, function (error, response, body) {
+
+    if (error) {
+        return console.log('Error occurred: ' + error);
+      }
+    
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  var output = JSON.parse(body);
+  var title =output.Title;
+  var year = output.Year;
+  var imbdRating=output.Ratings[0].Value;
+  var rottenRating=output.Ratings[1].Value;
+  var country= output.Country;
+  var lang= output.Language;
+  var plot= output.Plot;
+  var actors = output.Actors;
+
+  var movieData= (`Movie: ${title}\nMovie Year: ${year}\nIMDB Rating: ${imbdRating}\nRotten Tomatoes Rating: ${rottenRating}\nCountry where produced: ${country}\nLanguages: ${lang}\nPlot: ${plot}\nActors: ${actors}` );
+    
+  console.log(movieData);
+  appendToFile(movieData);
+
+
+
+
+});
 };
 
 function doThis(){
